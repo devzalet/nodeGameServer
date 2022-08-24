cac84cea5cd0deeb31dcec502ff7516770681288
devzalet <89173278+devzalet@users.noreply.github.com>
2022-08-23 23:25:21 -0400

    update30

---
+++ app.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+var userId = 0;
+io.on('connection', function(socket){
+  socket.userId = userId ++;
+  console.log('a user connected, user id: ' + socket.userId);
+
+  socket.on('chat', function(msg){
+    console.log('message from user#' + socket.userId + ": " + msg);
+    io.emit('chat', {
+      id: socket.userId,
+      msg: msg
+    });
+  });
+});
+
+
+//server.listen(process.env.PORT || 5000);
+http.listen(4000);
