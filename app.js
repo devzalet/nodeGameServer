2953e991a980fe297ada95ae47bf5adb0bb52751
devzalet <89173278+devzalet@users.noreply.github.com>
2022-08-24 03:17:59 -0400

    update35

--- app.js
+++ app.js
@@ -3,23 +3,37 @@ var http = require('http').Server(app);
 var mysql   =     require("mysql");
 var io = require('socket.io')(http);
 
+// // main();
+// // async function main(){
+//     const conexion = mysql.createPool({
+//         //connectionLimit   :   200,
+//         database: "nerachatsql",
+//         user: "p4tepc8qvn5e",
+//         host: "ku1p1sq5j8u6.eu-west-2.psdb.cloud",
+//         password: "pscale_pw_7IYtEK8IA4w9K3dM41ByZmw-Yoo1Gm6tDYX6FZTLgbs",
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+//     });
+//     //conexion.query('insert into mensajes values("otro","newmesaje")');
+//     console.log('se conecto correctamente a la bd');
+// //}
+
 // main();
 // async function main(){
     const conexion = mysql.createPool({
         //connectionLimit   :   200,
-        database: "nerachatsql",
-        user: "p4tepc8qvn5e",
-        host: "ku1p1sq5j8u6.eu-west-2.psdb.cloud",
-        password: "pscale_pw_7IYtEK8IA4w9K3dM41ByZmw-Yoo1Gm6tDYX6FZTLgbs",
-    // debug             :   false,
-    ssl: {
+        database: "heroku_700a51ffd0a558a",
+        user: "bfc2dbf35bd86e",
+        host: "eu-cdbr-west-03.cleardb.net",
+        password: "527862e8",
+            ssl: {
         rejectUnauthorized: false
     }
     });
     //conexion.query('insert into mensajes values("otro","newmesaje")');
     console.log('se conecto correctamente a la bd');
 //}
-
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -47,14 +61,17 @@ io.on('connection', function(socket) {
 
   socket.on('chat', function(msg){
     console.log('message from user#' + socket.userId + ": " + JSON.stringify(msg));
-    conexion.query('insert into mensajes values("otro","'+msg.name2+'")'); //from chat
+    //conexion.query('insert into mensajes values("otro","'+msg.name2+'")'); //from chat
+    
     //console.log('message from user#' + socket.userId + ": " + msg);
-    // conexion.query("INSERT INTO `mensajes` (`mensaje`,`otromensaje`) VALUES ('"+socket.userId+"','"+msg.name2+"')", function(err,rows){ //Insertando nuestro comentario
+    conexion.query("INSERT INTO `mensajes` (`mensaje`,`otromensaje`) VALUES ('"+socket.userId+"','"+msg.name2+"')")
+    // , function(err,rows){ //Insertando nuestro comentario
     //     connection.release();
     //     if(!err) {
     //       callback(true);
     //     }
     // });
+
     io.emit('chat', {
       id: socket.userId,
       //msg: msg
