d2815ca9a4234bf636167b2eeb2be3d5c62b1565
devzalet <89173278+devzalet@users.noreply.github.com>
2022-08-23 23:51:07 -0400

    updated25

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mysql   =     require("mysql"); //agregado
+
+// var conexion    =    mysql.createPool({
+//     connectionLimit   :   100,
+// 	database: "nerachatsql",
+// 	user: "21zqfflcphu3",
+// 	host: "ku1p1sq5j8u6.eu-west-2.psdb.cloud",
+// 	password: "pscale_pw_QypBkfdCIvpOzRBEXZxXc4rDv6XPTSPUz8wSDd_g7yc",
+// 	debug             :   false,
+// 	ssl: {
+// 	rejectUnauthorized: false
+// }
+// });
+
+main();
+async function main(){
+    const conexion = mysql.createPool({
+        //connectionLimit   :   200,
+        database: "nerachatsql",
+        user: "5107gyg8ioyw",
+        host: "ku1p1sq5j8u6.eu-west-2.psdb.cloud",
+        password: "pscale_pw_BtTO4xV33109V_DntPVi05q_-YMCBhHs-0iKn38fRqA",
+    // debug             :   false,
+    ssl: {
+        rejectUnauthorized: false
+    }
+    });
+    conexion.query('insert into mensajes values("otro","mensaje89101112")');
+    //console.log('se conecto correctamente a la bd');
+}
 
 
 app.get('/', function(req, res){
