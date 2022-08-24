d03e82f148b6e6168332cca96248e95c9bcd4a57
devzalet <89173278+devzalet@users.noreply.github.com>
2022-08-24 01:06:16 -0400

    update31

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
 var mysql   =     require("mysql"); //agregado
+var io = require('socket.io')(http);
 
-//main();
-//async function main(){
+// main();
+// async function main(){
     const conexion = mysql.createPool({
         //connectionLimit   :   200,
         database: "nerachatsql",
-        user: "5107gyg8ioyw",
+        user: "ecyhutv1dibx",
         host: "ku1p1sq5j8u6.eu-west-2.psdb.cloud",
-        password: "pscale_pw_BtTO4xV33109V_DntPVi05q_-YMCBhHs-0iKn38fRqA",
+        password: "pscale_pw_EvQSlZanut8DIhIqHg2DVVOrsFwzPOUgdhhURdX3axw",
     // debug             :   false,
     ssl: {
         rejectUnauthorized: false
     }
     });
-    //conexion.query('insert into mensajes values("otro","mensaje89101112")');
-    //console.log('se conecto correctamente a la bd');
+    //conexion.query('insert into mensajes values("otro","newmesaje")');
+    console.log('se conecto correctamente a la bd');
 //}
 
 
@@ -46,10 +46,19 @@ io.on('connection', function(socket) {
   console.log('a user connected, user id: ' + socket.userId);
 
   socket.on('chat', function(msg){
-    console.log('message from user#' + socket.userId + ": " + msg);
+    console.log('message from user#' + socket.userId + ": " + JSON.stringify(msg));
+    conexion.query('insert into mensajes values("otro","'+msg.name2+'")'); //from chat
+    //console.log('message from user#' + socket.userId + ": " + msg);
+    // conexion.query("INSERT INTO `mensajes` (`mensaje`,`otromensaje`) VALUES ('"+socket.userId+"','"+msg.name2+"')", function(err,rows){ //Insertando nuestro comentario
+    //     connection.release();
+    //     if(!err) {
+    //       callback(true);
+    //     }
+    // });
     io.emit('chat', {
       id: socket.userId,
-      msg: msg
+      //msg: msg
+      msg: msg.name2
     });
   });
 	// var currentPlayer = {};
