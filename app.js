204bcc7b352f052444357820e5246e56c7a64b23
devzalet <89173278+devzalet@users.noreply.github.com>
2022-08-24 09:33:11 -0400

    update60

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ io.on('connection', function(socket) {
             console.log(err);
             throw err;  
         } else{
-            console.log(result.displayName);
+            console.log(result[0].displayName);
         }
       });
   });
