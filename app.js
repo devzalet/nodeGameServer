2130fe42616e1bb6df22ac81d719f71b42e30923
devzalet <89173278+devzalet@users.noreply.github.com>
2022-08-24 00:07:06 -0400

    update30

--- app.js
+++ app.js
@@ -3,20 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mysql   =     require("mysql"); //agregado
 
-// var conexion    =    mysql.createPool({
-//     connectionLimit   :   100,
-// 	database: "nerachatsql",
-// 	user: "21zqfflcphu3",
-// 	host: "ku1p1sq5j8u6.eu-west-2.psdb.cloud",
-// 	password: "pscale_pw_QypBkfdCIvpOzRBEXZxXc4rDv6XPTSPUz8wSDd_g7yc",
-// 	debug             :   false,
-// 	ssl: {
-// 	rejectUnauthorized: false
-// }
-// });
-
-main();
-async function main(){
+//main();
+//async function main(){
     const conexion = mysql.createPool({
         //connectionLimit   :   200,
         database: "nerachatsql",
@@ -28,9 +16,9 @@ async function main(){
         rejectUnauthorized: false
     }
     });
-    conexion.query('insert into mensajes values("otro","mensaje89101112")');
+    //conexion.query('insert into mensajes values("otro","mensaje89101112")');
     //console.log('se conecto correctamente a la bd');
-}
+//}
 
 
 app.get('/', function(req, res){
@@ -38,8 +26,23 @@ app.get('/', function(req, res){
 });
 
 var userId = 0;
-io.on('connection', function(socket){
-  socket.userId = userId ++;
+// io.on('connection', function(socket){
+//   socket.userId = userId ++;
+//   console.log('a user connected, user id: ' + socket.userId);
+
+//   socket.on('chat', function(msg){
+//     console.log('message from user#' + socket.userId + ": " + msg);
+//     io.emit('chat', {
+//       id: socket.userId,
+//       msg: msg
+//     });
+//   });
+// });
+
+//ADDED
+io.on('connection', function(socket) {
+	
+    socket.userId = userId ++;
   console.log('a user connected, user id: ' + socket.userId);
 
   socket.on('chat', function(msg){
@@ -49,8 +52,41 @@ io.on('connection', function(socket){
       msg: msg
     });
   });
-});
+	// var currentPlayer = {};
+	// currentPlayer.name = 'unknown';
 
+	socket.on('newmsg',function(data){
+		console.log(currentPlayer.name+' recv: play: '+JSON.stringify(data));
+		addComentario(data,function(res){
+		  if(res){
+			  io.emit('refrescar',data);
+		  } else {
+			  io.emit('error');
+		  }
+		});
+    });
+});
+var addComentario = function (data,callback) {
+    conexion.getConnection(function(err,connection){
+        if (err) {
+          connection.release();
+          callback(false);
+          return;
+        }
+    connection.query("INSERT INTO `mensajes` (`mensaje`,`otromensaje`) VALUES ('"+data.name+"','"+data.name2+"')", function(err,rows){ //Insertando nuestro comentario
+            connection.release();
+            if(!err) {
+              callback(true);
+            }
+        });
+     connection.on('error', function(err) {
+              callback(false);
+              return;
+        });
+    });
+}
+
+//ADEED
 
 http.listen(process.env.PORT || 5000);
 //http.listen(4000);
